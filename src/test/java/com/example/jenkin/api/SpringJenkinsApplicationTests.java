f026d475126fdbc4e901fef462f1bd6002ae06f4
Govardhan <govardhanmk06@gmail.com>
2026-07-27 01:12:12 +0530

    first git commit

--- src/test/java/com/example/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkin/api/SpringJenkinsApplicationTests.java
@@ -1,10 +1,23 @@
 package com.example.jenkin.api;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-class SpringJenkinsApplicationTests {
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinsApplicationTests {
+   
+	 public static  Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info("===Test case Executing====");
+		
+		assertEquals(true,true);
 	}
 }
