463c49f487ea9cb16ceebe1c32a58f52bce94423
Govardhan <govardhanmk06@gmail.com>
2026-07-27 01:20:15 +0530

    fix junit5 test

--- src/test/java/com/example/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkin/api/SpringJenkinsApplicationTests.java
@@ -1,15 +1,12 @@
 package com.example.jenkin.api;
 
-import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJenkinsApplicationTests {
    
